5b118d9b99d34fd835469753d8333d46629c1620
AlexArmijoAcevedo <149970806+AlexArmijoAcevedo@users.noreply.github.com>
2025-05-18 09:57:08 -0400

    Se añade base de datos al login y registro, se añade conexion a base de datos de productos

--- ferremas/taller_ferremas/views.py
+++ ferremas/taller_ferremas/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Producto,Pedido
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+
 
 def index(request):
-    return render(request, 'index.html')
+    productos = Producto.objects.all()
+    return render(request, 'index.html',{'productos':productos})
 def login(request):
     return render(request, 'login.html')
+def agregar_producto(request):
+    return render(request, 'agregar_producto.html')
+
+def registrar_pedido(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        # uid_usuario, productos, direccion, total
+        Pedido.objects.create(**data)
+        return JsonResponse({'ok': True})
