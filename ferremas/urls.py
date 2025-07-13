3351e3b89aed088dadcb308ba563b56bbe611c61
AlexArmijoAcevedo <149970806+AlexArmijoAcevedo@users.noreply.github.com>
2025-07-13 17:32:48 -0400

    urls.py actualizado

--- ferremas/urls.py
+++ ferremas/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('taller_ferremas.urls')),
+    path('', include('ferremas.taller_ferremas.urls')),
 ]
