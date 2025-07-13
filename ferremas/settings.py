125c6d850536a4a59f43b463936e9cfde66440cc
AlexArmijoAcevedo <149970806+AlexArmijoAcevedo@users.noreply.github.com>
2025-07-13 17:00:10 -0400

    settings commit

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ferremas.taller_ferremas',
+    'taller_ferremas',
 ]
 
 # Middleware
